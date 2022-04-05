da5c9d0ad7ea3fd39bed072d2be254b221b7a3d2
gmatteo <gmatteo@gmail.com>
2022-04-06 02:48:36 +0300

    Add Flow results

---
+++ flow_pseudo_ref/w0/t0/job.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --partition=small
+#SBATCH --job-name=w0_t0
+#SBATCH --ntasks=32
+#SBATCH --mem-per-cpu=2048
+#SBATCH --time=0-12:0:0
+#SBATCH --account=project_465000061
+#SBATCH --output=/pfs/lustrep2/scratch/project_465000061/magianto/H_pseudo/flow_pseudo/w0/t0/queue.qout
+#SBATCH --error=/pfs/lustrep2/scratch/project_465000061/magianto/H_pseudo/flow_pseudo/w0/t0/queue.qerr
+cd /pfs/lustrep2/scratch/project_465000061/magianto/H_pseudo/flow_pseudo/w0/t0
+# OpenMp Environment
+export OMP_NUM_THREADS=1
+# Commands before execution
+source $HOME/git_repos/abinit/_build_gnu/modules.sh
+
+srun  -n 32 abinit --timelimit 0-12:0:0 < /pfs/lustrep2/scratch/project_465000061/magianto/H_pseudo/flow_pseudo/w0/t0/run.files > /pfs/lustrep2/scratch/project_465000061/magianto/H_pseudo/flow_pseudo/w0/t0/run.log 2> /pfs/lustrep2/scratch/project_465000061/magianto/H_pseudo/flow_pseudo/w0/t0/run.err
--- p.py
+++ p.py
@@ -8,18 +8,18 @@ def main():
     ae_vols, ae_enes = np.loadtxt("ae_eos.txt").T
     #print(ae_vols)
 
-    #eos_json = sys.argv[1]
-    #with open(eos_json, "rt") as fh:
-    #    data = json.load(fh)
-    #    abi_vols = data["volumes_ang3"]
-    #    abi_enes = data["energies_ev"]
-
+    eos_json = sys.argv[1]
+    natom = 2
+    with open(eos_json, "rt") as fh:
+        data = json.load(fh)
+        abi_vols = np.array(data["volumes_ang3"]) / natom 
+        abi_enes = np.array(data["energies_ev"]) / natom
 
     from abipy.tools.plotting import (set_axlims, add_fig_kwargs, get_ax_fig_plt, get_axarray_fig_plt)
     ax, fig, plt = get_ax_fig_plt()
     ax.plot(ae_vols, ae_enes - ae_enes.min(), color="red", marker="o", label="AE")
 
-    #ax.plot(abi_vols, abi_enes - abi_enes.min(), color="blue", marker="x", label="Abinit")
+    ax.plot(abi_vols, abi_enes - abi_enes.min(), color="blue", marker="x", label="Abinit")
 
     ax.set_ylabel('Energy (eV)')
     ax.set_xlabel('Volume')
